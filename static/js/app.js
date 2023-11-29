3ddb202cab02483f6760d167bcc0cd9c6e830504
root <root@PATRYK-NFHTHTE0>
2023-11-28 21:01:12 -0500

    Single Page Setup

--- static/js/app.js
+++ static/js/app.js
@@ -1,169 +1,79 @@
-let zoff = 0;
-let flowfield = [];
-const resolution = 99;
-let particles = [];
-let cols, rows;
-const maxDistance = 55; // Maximum distance for connecting lines
+document.addEventListener('DOMContentLoaded', function () {
+    const heroContainer = document.querySelector('.hero-container');
+    const contentContainer = document.querySelector('.content-container');
+    const navBar = document.querySelector('.nav-bar');
+    const navArrow = document.querySelector('.nav-arrow');
 
-function setup() {
-    // Select the container
-    let container = document.querySelector('.canvas-container');
-  
-    // Get the width and height of the container
-    let containerWidth = container.clientWidth;
-    let containerHeight = container.clientHeight;
-  
-    // Create the canvas with the size of the container
-    console.log(containerHeight, containerWidth)
-    let cnv = createCanvas(containerWidth, containerHeight);
-
-    console.log(cnv)
-  
-    // Attach the canvas to the container
-    cnv.parent(container);
-
-    // Rest of your setup code
-    colorMode(HSB, 360, 255, 255, 100);
-    cols = floor(width / resolution);
-    rows = floor(height / resolution);
-    flowfield = new Array(cols * rows);
-    initParticles();
-}
-
-  
-
-function initParticles() {
-  particles = [];
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      particles.push(new Particle(i * resolution, j * resolution));
+    function fetchTemplate(url, container) {
+        fetch(url)
+            .then(response => response.text())
+            .then(html => {
+                container.innerHTML = '';
+                container.innerHTML = html;
+            })
+            .catch(error => {
+                console.error('Error fetching template:', error);
+            });
     }
-  }
-}
 
-function draw() {
-  background(255);
-  updateFlowField();
-  for (let particle of particles) {
-    particle.follow(flowfield);
-    particle.update();
-    particle.edges();
-    particle.show();
-  }
-  connectParticles();
-}
+    function handleScroll() {
+        const scrollPositionVh = (window.scrollY / window.innerHeight) * 100;
+        navBar.style.transform = scrollPositionVh >= 30 ? 'translateY(-100%)' : 'translateY(0)';
+        navArrow.style.transform = scrollPositionVh >= 30 ? 'translateY(-150%)' : 'translateY(0)';
+    }
 
-function connectParticles() {
-  stroke(0); // Black color for the lines
-  strokeWeight(1); // Very thin lines
+    function handleMouseOver(event) {
+        if (event.target.tagName === 'IMG') {
+            const imageContainer = event.target.parentElement;
+            const projectTitle = imageContainer.parentElement.getAttribute('data-project-title');
 
-  // Array to keep track of the number of connections for each particle
-  let connections = Array(particles.length).fill(0);
-
-  for (let a = 0; a < particles.length; a++) {
-    for (let b = 0; b < particles.length; b++) {
-      // Ensure different particles and less than 5 connections for particle 'a'
-      if (a !== b && connections[a] < 3) {
-        let dist = p5.Vector.dist(particles[a].pos, particles[b].pos);
-        if (dist < maxDistance) {
-          line(particles[a].pos.x, particles[a].pos.y, particles[b].pos.x, particles[b].pos.y);
-          connections[a]++; // Increment connection count for particle 'a'
+            if (!imageContainer.querySelector('.project-card-hover')) {
+                const hoverElement = document.createElement('a');
+                hoverElement.textContent = projectTitle;
+                hoverElement.className = 'project-card-hover';
+                imageContainer.appendChild(hoverElement);
+            }
         }
-      }
     }
-  }
-}
 
-function windowResized() {
-    // Select the container
-    let container = document.querySelector('.canvas-container');
-  
-    // Get the width and height of the container
-    let containerWidth = container.clientWidth;
-    let containerHeight = container.clientHeight;
-  
-    // Resize the canvas to the container's size
-    resizeCanvas(containerWidth, containerHeight);
-  
-    // Recalculate columns and rows based on new canvas size
-    let newCols = floor(width / resolution);
-    let newRows = floor(height / resolution);
-  
-    // Re-initialize particles if the grid size changes
-    if (newCols !== cols || newRows !== rows) {
-      cols = newCols;
-      rows = newRows;
-      flowfield = new Array(cols * rows);
-      initParticles();
+    function handleMouseOut(event) {
+        if (event.target.tagName === 'IMG') {
+            const imageContainer = event.target.parentElement;
+            const hoverElement = imageContainer.querySelector('.project-card-hover');
+
+            if (hoverElement) {
+                // Delay the removal of hoverElement
+                setTimeout(() => {
+                    hoverElement.remove();
+                }, 150);
+            }
+        }
     }
-  }
-  
 
-function updateFlowField() {
-  let scale = 0.1;
-  let xoff = 0;
-  for (let i = 0; i < cols; i++) {
-    let yoff = 0;
-    for (let j = 0; j < rows; j++) {
-      const index = i + j * cols;
-      const theta = map(noise(xoff, yoff, zoff), 0, 1, 0, TWO_PI);
-      flowfield[index] = p5.Vector.fromAngle(theta);
-      yoff += scale;
+    function handleProjectLinkClick(event){
+        if (event.target.tagName === 'A' && event.target.classList.contains("project-card-hover")) {
+            console.log("let's go to that project", event.target)
+
+
+
+        }
     }
-    xoff += scale;
-  }
-  zoff += 0.01;
-}
 
-class Particle {
-  constructor(x, y) {
-    this.pos = createVector(x, y);
-    this.prevPos = this.pos.copy();
-    this.vel = createVector();
-    this.acc = createVector();
-  }
 
-  follow(vectors) {
-    let x = floor(this.pos.x / resolution);
-    let y = floor(this.pos.y / resolution);
-    let index = x + y * cols;
-    let force = vectors[index];
-    this.applyForce(force);
-  }
+    fetchTemplate('templates/home_hero.html', heroContainer);
+    fetchTemplate('templates/home_content.html', contentContainer);
 
-  applyForce(force) {
-    this.acc.add(force);
-  }
+    contentContainer.addEventListener('click', handleProjectLinkClick);
 
-  update() {
-    this.vel.add(this.acc);
-    this.vel.limit(2); // Limiting the velocity to slow down the movement
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-  }
+    window.addEventListener('scroll', handleScroll);
+    contentContainer.addEventListener('mouseover', handleMouseOver);
+    contentContainer.addEventListener('mouseout', handleMouseOut);
 
-  edges() {
-    if (this.pos.x > width) {
-      this.pos.x = 0;
-      this.prevPos.x = this.pos.x;
-    }
-    if (this.pos.x < 0) {
-      this.pos.x = width;
-      this.prevPos.x = this.pos.x;
-    }
-    if (this.pos.y > height) {
-      this.pos.y = 0;
-      this.prevPos.y = this.pos.y;
-    }
-    if (this.pos.y < 0) {
-      this.pos.y = height;
-      this.prevPos.y = this.pos.y;
-    }
-  }
-
-  show() {
-    stroke(0);
-    strokeWeight(1);
-    point(this.pos.x, this.pos.y);
-  }
-}
+    document.querySelector('a[href="#nav-top"]').addEventListener('click', function (e) {
+        e.preventDefault(); // Prevent the default anchor behavior
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Smooth scroll to the top
+        });
+    });
+});
---
+++ static/js/heroSeparation.js
@@ -0,0 +1,197 @@
+let zoff = 0;
+let flowfield = [];
+const resolution = 99;
+let particles = [];
+let cols, rows;
+const maxDistance = 55; // Maximum distance for connecting lines
+
+function setup() {
+    // Select the container
+    let container = document.querySelector('.canvas-container');
+  
+    // Get the width and height of the container
+    let containerWidth = container.clientWidth;
+    let containerHeight = container.clientHeight;
+  
+    // Create the canvas with the size of the container
+    console.log(containerHeight, containerWidth)
+    let cnv = createCanvas(containerWidth, containerHeight);
+
+    console.log(cnv)
+  
+    // Attach the canvas to the container
+    cnv.parent(container);
+
+    // Rest of your setup code
+    colorMode(HSB, 360, 255, 255, 100);
+    cols = floor(width / resolution);
+    rows = floor(height / resolution);
+    flowfield = new Array(cols * rows);
+    initParticles();
+}
+
+  
+
+function initParticles() {
+  particles = [];
+  let particleSize = random(5, 20); // Define the size of each particle
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+    particles.push(new Particle(i * resolution, j * resolution, particleSize));
+    }
+  }
+}
+
+function draw() {
+  background(255);
+  updateFlowField();
+  for (let particle of particles) {
+    particle.follow(flowfield);
+    let separationForce = particle.separate(particles);
+    particle.applyForce(separationForce);
+    particle.update();
+    particle.edges();
+    particle.show();
+  }
+  connectParticles();
+}
+
+function connectParticles() {
+  stroke(80); // Black color for the lines
+  strokeWeight(1); // Very thin lines
+
+  // Array to keep track of the number of connections for each particle
+  let connections = Array(particles.length).fill(0);
+
+  for (let a = 0; a < particles.length; a++) {
+    for (let b = 0; b < particles.length; b++) {
+      // Ensure different particles and less than 5 connections for particle 'a'
+      if (a !== b && connections[a] < 3) {
+        let dist = p5.Vector.dist(particles[a].pos, particles[b].pos);
+        if (dist < maxDistance) {
+          line(particles[a].pos.x, particles[a].pos.y, particles[b].pos.x, particles[b].pos.y);
+          connections[a]++; // Increment connection count for particle 'a'
+        }
+      }
+    }
+  }
+}
+
+function windowResized() {
+    // Select the container
+    let container = document.querySelector('.canvas-container');
+  
+    // Get the width and height of the container
+    let containerWidth = container.clientWidth;
+    let containerHeight = container.clientHeight;
+  
+    // Resize the canvas to the container's size
+    resizeCanvas(containerWidth, containerHeight);
+  
+    // Recalculate columns and rows based on new canvas size
+    let newCols = floor(width / resolution);
+    let newRows = floor(height / resolution);
+  
+    // Re-initialize particles if the grid size changes
+    if (newCols !== cols || newRows !== rows) {
+      cols = newCols;
+      rows = newRows;
+      flowfield = new Array(cols * rows);
+      initParticles();
+    }
+  }
+  
+
+function updateFlowField() {
+  let scale = 0.1;
+  let xoff = 0;
+  for (let i = 0; i < cols; i++) {
+    let yoff = 0;
+    for (let j = 0; j < rows; j++) {
+      const index = i + j * cols;
+      const theta = map(noise(xoff, yoff, zoff), 0, 1, 0, TWO_PI);
+      flowfield[index] = p5.Vector.fromAngle(theta);
+      yoff += scale;
+    }
+    xoff += scale;
+  }
+  zoff += 0.01;
+}
+
+class Particle {
+  constructor(x, y, size) {
+    this.pos = createVector(x, y);
+    this.prevPos = this.pos.copy();
+    this.vel = createVector();
+    this.acc = createVector();
+    this.r = size; // radius or size of the particle
+  }
+
+  follow(vectors) {
+    let x = floor(this.pos.x / resolution);
+    let y = floor(this.pos.y / resolution);
+    let index = x + y * cols;
+    let force = vectors[index];
+    this.applyForce(force);
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  separate(particles) {
+    let desiredseparation = this.r * 2.1;
+    let steer = createVector();
+    let count = 0;
+    for (let other of particles) {
+      let d = p5.Vector.dist(this.pos, other.pos);
+      if ((d > 0) && (d < desiredseparation)) {
+        let diff = p5.Vector.sub(this.pos, other.pos);
+        diff.normalize();
+        diff.div(d); // Weight by distance
+        steer.add(diff);
+        count++;
+      }
+    }
+    if (count > 0) {
+      steer.div(count);
+      steer.normalize();
+      steer.mult(2); // Assuming 2 is the max speed
+      steer.sub(this.vel);
+      steer.limit(0.9); // Assuming 0.1 is the max force
+    }
+    return steer;
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.vel.limit(1.1); // Limiting the velocity to slow down the movement
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  edges() {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.prevPos.x = this.pos.x;
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.prevPos.x = this.pos.x;
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.prevPos.y = this.pos.y;
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.prevPos.y = this.pos.y;
+    }
+  }
+
+  show() {
+    stroke(80);
+    strokeWeight(10);
+    point(this.pos.x, this.pos.y);
+  }
+}
