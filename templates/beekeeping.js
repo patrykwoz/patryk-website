a61611816356d7ba90740b77307183b6a3226ac3
root <root@PATRYK-NFHTHTE0>
2023-12-13 20:01:39 -0500

    Updated Caption Beekeeping.

--- templates/beekeeping.js
+++ templates/beekeeping.js
@@ -9,7 +9,7 @@ beekeepingReference.innerHTML = `
     <div class="reference-content">
         <img class="reference-image" src="assets/beekeeping/IMG 076.jpg"></img>
         <p class="reference-citation">
-            Inspecting a bee hive.
+            Checking up on the hive near my parents' home in Poland, 2018.
         </p>
     </div>
 `;
